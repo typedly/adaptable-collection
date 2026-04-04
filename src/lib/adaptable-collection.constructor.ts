d01dc4bcdf14603df03f3644f01ab84650097338
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-04-04 22:17:27 +0200

    refactor(`AdaptableCollectionConstructor`): reorder `T` before `E`

--- src/lib/adaptable-collection.constructor.ts
+++ src/lib/adaptable-collection.constructor.ts
@@ -2,8 +2,8 @@
 import type { AdaptableCollectionShape } from './adaptable-collection.shape';
 import type { CollectionSettings } from '@typedly/collection';
 import type { ConfigurableCollectionAdapter } from '@typedly/configurable-collection';
-// Type.
 import type { ConstrainedConstructor } from '@typedly/constructor';
+// Type.
 import type { InferAsyncOf } from '@typedly/data';
 import type { InferCollectionAdapter } from './infer-collection-adapter.type';
 import type { InferCollectionType, InferElementFromSettings } from '@typedly/collection';
@@ -15,8 +15,8 @@ import type { InferSettings } from '@typedly/configurable-data';
  * @template {AdaptableCollectionShape<A, C, T, E, R>} [S=InferCollectionAdapter<S>] The type of adaptable collection, inferred from the collection adapter specified in the adaptable collection shape.
  * @template {ConfigurableCollectionAdapter<C, T, E, R>} [A=InferCollectionAdapter<S>] The type of the collection adapter, inferred from the adaptable collection shape or defaults to `ConfigurableCollectionAdapter` if it cannot be inferred.
  * @template {CollectionSettings<T, E, R>} [C=InferSettings<S>] The collection settings type, inferred from the adaptable collection shape or the collection adapter, which defines the configuration for the collection.
- * @template [E=InferElementFromSettings<C>] The type of elements in the collection, inferred from the collection settings.
  * @template {Iterable<E>} [T=InferCollectionType<C>] The type of the iterable collection, inferred from the collection settings.
+ * @template [E=InferElementFromSettings<C>] The type of elements in the collection, inferred from the collection settings.
  * @template {boolean} [R=InferAsyncOf<[C, A]>] The type indicating whether the collection is asynchronous, inferred from the collection settings and adapter.
  * @extends {ConstrainedConstructor<AdaptableCollectionShape<A, C, T, E, R>, S, [C, A, S, ...E[]]>}
  */
@@ -24,8 +24,8 @@ export interface AdaptableCollectionConstructor<
   S extends AdaptableCollectionShape<A, C, T, E, R>,
   A extends ConfigurableCollectionAdapter<C, T, E, R> = InferCollectionAdapter<S>,
   C extends CollectionSettings<T, E, R> = InferSettings<S>,
-  E = InferElementFromSettings<C>,
   T extends Iterable<E> = InferCollectionType<C>,
+  E = InferElementFromSettings<C>,
   R extends boolean = InferAsyncOf<[C, A]>,
 > extends ConstrainedConstructor<
   AdaptableCollectionShape<A, C, T, E, R>,
